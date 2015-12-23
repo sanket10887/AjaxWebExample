f4620b576ac43783280dd25af8e64220ea5faaf6
mklaniya <mk.laniya@gmail.com>
2015-12-23 18:23:30 +0530

    first changes

--- AjaxFileUpload/src/com/programmingfree/fileupload/UploadFile.java
+++ AjaxFileUpload/src/com/programmingfree/fileupload/UploadFile.java
@@ -34,7 +34,7 @@ public class UploadFile extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("doGet");
+		
 	}
 
 	/**
