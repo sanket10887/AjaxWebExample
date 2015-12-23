e1d0a06a164365d5c83e6662ab00794012d65e1b
Sanket <sanket10887@gmail.com>
2015-12-23 16:42:17 +0530

    first changes

--- AjaxFileUpload/src/com/programmingfree/fileupload/UploadFile.java
+++ AjaxFileUpload/src/com/programmingfree/fileupload/UploadFile.java
@@ -1,15 +1,14 @@
 package com.programmingfree.fileupload;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import java.io.File;
-import java.util.Iterator;
-import java.util.List;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
@@ -42,9 +41,7 @@ public class UploadFile extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		  boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-			System.out.println("------------------------");
-			System.out.println(isMultipart);
-	        if (isMultipart) {
+			if (isMultipart) {
 	        	// Create a factory for disk-based file items
 	        	FileItemFactory factory = new DiskFileItemFactory();
 
